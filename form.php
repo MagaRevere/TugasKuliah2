bf5e04786be2201c3ef5bc5e4851bfe1ece3cce6
MagaRevere <ramasanjaya67@gmail.com>
2021-11-16 22:51:07 +0700

    TugasKuliah2

---
+++ form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Form Input Barang</title>
+</head>
+<body>
+<h2>Form Input Data Barang</h2>
+ <form method = 'post' action= "simpan.php">
+ <table>
+ <tr>
+<td>Kode Barang</td>
+<td>:</td>
+<td><input type='text' name='KdBarang'></td>
+ </tr>
+ <tr>
+<td>Nama Barang</td>
+<td>:</td>
+<td><input type='text' name='NamaBarang'></td>
+ </tr>
+ <tr>
+<td>Harga</td>
+<td>:</td>
+<td><input type='number' name='Harga'></td>
+ </tr>
+ <tr></tr>
+ <tr></tr>
+ <tr></tr>
+ <tr>
+<td></td>
+<td></td>
+<td><input type='submit' value='SIMPAN'></td>
+ </tr>
+ </table>
+ </form>
+</body>
+</html>
---
+++ input_barang.php
@@ -0,0 +1,35 @@
+<?php
+include("koneksi.php");
+
+if(isset($_POST['save'])){
+    $query_input=mysqli_query($koneksi,"insert into anggota (nama, alamat, no_telp)
+    '".$_POST['nama']."',
+    '".$_POST['alamat']."',
+    '".$_POST['no_telp']."')");
+
+    if($query_input) {
+        header('location:view_kategori.php');
+    }else{
+        echo mysqli_error();
+}
+}
+?>
+<form method="POST">
+<table class="table table-bordered" border="1">
+    <tr>
+        <td>Nama</td>
+        <td><input type="text" name="nama" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Alamat</td>
+        <td><input type="text" name="alamat" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>No Telpon</td>
+        <td><input type="text" name="no_telp" class="form-control"></td>
+    </tr>
+        <tr>
+            <td><input type="submit" name="save" class="btn btn-danger"></td>
+        </tr>
+        </table>
+        </form>
